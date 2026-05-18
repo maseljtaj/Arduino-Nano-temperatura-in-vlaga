85b7a08e460fc2e1c71848dc59f41631d30a432f
maseljtaj <maseljtaj@gmail.com>
2026-05-18 11:51:11 +0200

    Update temp_vlaga_oled_dht22.ino

--- code/temp_vlaga_oled_dht.ino
+++ code/temp_vlaga_oled_dht.ino
@@ -3,42 +3,76 @@
 #include <Adafruit_SSD1306.h>
 #include <DHT.h>
 
+// OLED nastavitve
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-#define DHTPIN 3
-#define DHTTYPE DHT11
+#define OLED_RESET -1
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// DHT22 nastavitve
+#define DHTPIN 10
+#define DHTTYPE DHT22
+
 DHT dht(DHTPIN, DHTTYPE);
 
-float tempSmooth = 0;
-float humSmooth = 0;
-
 void setup() {
+
+  // zagon OLED
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    while (true);
+  }
+
+  // zagon DHT22
   dht.begin();
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+
+  // začetni ekran
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+
+  display.setTextSize(2);
+  display.setCursor(10, 20);
+  display.println("ZAGON");
+
+  display.display();
+
+  delay(2000);
 }
+
 void loop() {
 
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
-  if (isnan(t) || isnan(h)) return;
-  
-  // ===== SMOOTHING =====
-  tempSmooth = (tempSmooth * 0.8) + (t * 0.2);
-  humSmooth  = (humSmooth * 0.8) + (h * 0.2);
+  delay(3000);
+
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
 
   display.clearDisplay();
+
+  // če DHT22 ne dela
+  if (isnan(temp) || isnan(hum)) {
+
+    display.setTextSize(1);
+    display.setCursor(0, 25);
+    display.println("DHT22 ERROR");
+
+    display.display();
+    return;
+  }
+
+  // temperatura
   display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 5);
-  display.print("T:");
-  display.print(tempSmooth,1);
-  display.print((char)247);
+  display.setCursor(0, 0);
+
+  display.print("Temp:");
+  display.print(temp, 1);
   display.print("C");
+
+  // vlaga
   display.setCursor(0, 35);
-  display.print("H:");
-  display.print(humSmooth,0);
+
+  display.print("Vlaga:");
+  display.print(hum, 0);
   display.print("%");
+
   display.display();
-  delay(2000);
 }
